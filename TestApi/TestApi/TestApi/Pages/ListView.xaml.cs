f6147e7e5d0bb62f2dc3258dd400592e20410b37
Huy Đào <huydqth2009022@fpt.edu.vn>
2021-11-19 10:58:31 +0700

    Handling saving and retrieving data from the database with universal app

---
+++ TestApi/TestApi/TestApi/Pages/ListView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TestApi.Service;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestApi.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ListView : Page
+    {
+        private PhotoService photoService = new PhotoService();
+
+        public ListView()
+        {
+            this.InitializeComponent();
+            this.Loaded += ListPhoto_LoadedAsync;
+        }
+
+        private async void ListPhoto_LoadedAsync(object sender, RoutedEventArgs e)
+        {
+            var ListSong = await photoService.GetPhotoAsync();
+            MyListView.ItemsSource = ListSong;
+        }
+    }
+}
---
+++ TestApi/TestApi/TestApi/Service/PhotoService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TestApi.Entities;
+
+namespace TestApi.Service
+{
+    class PhotoService
+    {
+        private const string ApiBaseUrl = "https://jsonplaceholder.typicode.com";
+        private const string ApiSongPath = "/photos";
+
+        public async Task<List<Photo>> GetPhotoAsync()
+        {
+
+            List<Photo> result = new List<Photo>();
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                var requestConnection =
+                    await httpClient.GetAsync(ApiBaseUrl + ApiSongPath);
+                if (requestConnection.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+
+                    var content = await requestConnection.Content.ReadAsStringAsync();
+                    result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Photo>>(content);
+                    Console.WriteLine(content);
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+    }
+}
